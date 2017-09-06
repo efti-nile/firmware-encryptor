a38680105f3c8e721d5b3d98c31c1605ef49ba06
efti-nile <efti.nile@gmail.com>
2017-09-06 23:36:29 +0400

    Changes flasher module

--- linboot_flasher.py
+++ linboot_flasher.py
@@ -8,11 +8,8 @@ import sys
 
 
 class LinbootFlasher(serial.Serial):
-    cmd_opcodes = {"version": b'\x01', "write_page": b'\x02', "init_cipher": b'\x03'}
-    ans_opcodes = {"cipher_initialized": b'\x01'}
-    version_string_maxlen = 64
-    answer_header_len = 3
-    command_header_len = 3
+    cmd_opcodes = {"version": b'\x01', "init cypher": b'\x02', "write page": b'\x03'}
+    message_header_len = 3
 
     def __init__(self, *args, framesize=144):
         super().__init__(*args, timeout=1)
@@ -29,31 +26,26 @@ class LinbootFlasher(serial.Serial):
     def flash(self, fw_binfile):
         with open(fw_binfile, "rb") as fw:
             fw_image = fw.readall()
-        if (len(fw_image) - 4) % self.framesize != 0:
-            raise ValueError("Incorrect length of firmware binary file")
-        self.__send_message(self.cmd_opcodes["init_cipher"])
-        ans = self.read(4)
-        if len(ans < 4) or ans[2] != self.ans_opcodes["cipher_initialized"] or ans[3] != crc8(ans[:-1]):
-            sys.stderr.write("[ERROR  ] No ACK after cipher initialization {0}\n")
-            return False
-        for frame, frame_no in (fw_image[i*self.framesize:(i+1)*self.framesize], i
-                                for i in range(len(fw_image)//self.framesize)):
-            self.__send_message(self.cmd_opcodes["write_page"], frame)
-            ans = self.read(2)
-            if len(ans) < 2:
-                sys.stderr.write("[ERROR  ] No ACK while flashing frame no. {0}\n".format(frame_no))
+        if len(fw_image) % self.framesize != 0:
+            sys.stderr.write("[ERROR  ] Invalid firmware binary file\n")
+            return
+        if self.__command(self.cmd_opcodes["init cipher"]) is None:
+            sys.stderr.write("[ERROR  ] No cipher initialization ACK\n")
+            return
+        for frame, frame_no in tqdm((fw_image[i*self.framesize:(i+1)*self.framesize], i
+                                     for i in range(len(fw_image)//self.framesize))):
+            if self.__command(self.cmd_opcodes["write page"], frame):
+                sys.stderr.write("[ERROR  ] No written flash page ACK. Page No. {0}\n".format(frame_no))
+                return
 
     def version(self):
-        self.__send_message(self.cmd_opcodes["version"])
-        ans = self.read(self.version_string_maxlen)
-        datalen = ans[0]
-        if len(ans) < 4 or crc8(version_string[:-1]) != version_string[-1]:
-            sys.stderr.write("[ERROR  ] No answer on version string request")
-            return ""
-        if crc8
-        return version_string.decode("utf-8", "ignore")
+        return self.__command(self.cmd_opcodes["version"])
 
-    def __send_message(self, opcode, data=b''):
+    def __command(self, opcode, data=b'', expected_answer_datalen=None):
+        self.__send_message(opcode, data)
+        return self.__receive_answer(opcode, expected_answer_datalen)
+
+    def __send_message(self, opcode, data):
         assert isinstance(self.lin_address, bytes) and len(self.lin_address) == 1
         assert isinstance(data, bytes) and len(data) < 256
         assert isinstance(opcode, bytes) and len(opcode) == 1
@@ -61,16 +53,28 @@ class LinbootFlasher(serial.Serial):
         message = message_header + data
         message += bytes([crc8(message)])
         self.write(message)
-        # self.read(len(message))  # skip echoed bytes (bytes echo because-of LIN)
+        self.read(len(message))  # skip echoed bytes (bytes echo because-of LIN)
 
-    def __receive_answer(self, opcode, expected_datalen=0):
-        bytes_to_receive = self.command_header_len + self.expected_datalen + 1
+    def __receive_answer(self, expected_opcode, expected_datalen=None):
+        assert isinstance(expected_opcode, bytes) and len(opcode) == 1
+        if expected_datalen is not None:
+            bytes_to_receive = self.command_header_len + self.expected_datalen + 1
+        else:
+            bytes_to_receive = self.answer_maxlen
         answer = self.read(bytes_to_receive)
-        if len(answer) < bytes_to_receive:
-            sys.stderr.write("[ERROR  ] Received less than expected\n")
+        if len(answer) < self.answer_header_len:
+            sys.stderr.write("[ERROR  ] Header not received\n")
             return None
-        if bytes([crc8(answer[:-1])]) != answer[-1]:
+        datalen = ord(answer[0])
+        answer = answer[:self.command_header_len + datalen]
+        if crc8(answer[:-1]) != ord(answer[-1]):
             sys.stderr.write("[ERROR  ] CRC check failed\n")
             return None
-        return answer[self.command_header_len:self.command_header_len+]
-
+        if expected_datalen is not None and datalen != expected_datalen:
+            sys.stderr.write("[ERROR  ] Met not expected data length: {0}\n".format(datalen))
+            return None
+        opcode = answer[1]
+        if opcode != expected_opcode:
+            sys.stderr.write("[ERROR  ] Met not expected opcode: {0}\n".format(opcode))
+            return None
+        return answer[self.command_header_len:self.command_header_len + datalen]
